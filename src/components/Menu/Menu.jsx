b6a68eee4ac8bbd2a9a4a2da19f38e4acee8e3e2
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-17 13:16:47 +0300

    add Navigation component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,41 @@
 import { Link } from 'react-router-dom';
+import css from './Menu.module.css';
+import { useLocation } from 'react-router-dom';
 
 const Menu = () => {
+  const location = useLocation();
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/catalog">Catalog</Link>
-      <Link to="/favorites">Favorites</Link>
+    <div className={css.navPanel}>
+      <nav className={css.navigation}>
+        <Link
+          className={[
+            css.navItem,
+            location.pathname === '/' && css.current,
+          ].join(' ')}
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className={[
+            css.navItem,
+            location.pathname === '/catalog' && css.current,
+          ].join(' ')}
+          to="/catalog"
+        >
+          Catalog
+        </Link>
+        <Link
+          className={[
+            css.navItem,
+            location.pathname === '/favorites' && css.current,
+          ].join(' ')}
+          to="/favorites"
+        >
+          Favorites
+        </Link>
+      </nav>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,124 @@ const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ducimus
+        dicta voluptatibus perspiciatis dolorum laboriosam amet quod quam
+        recusandae id? Maxime illum excepturi rerum aperiam et ab, consequatur
+        quod sequi.
+      </div>
+
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        modi veritatis esse laudantium suscipit iusto itaque doloribus
+        perferendis incidunt id sunt saepe velit, deleniti fugit dignissimos
+        voluptates quisquam amet assumenda.
+      </div>
+      <div>HomePage</div>
     </>
   );
 };
