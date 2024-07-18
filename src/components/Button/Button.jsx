d0f5ca09650759f7e5fe75e8d8698c849c0cc729
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-18 09:33:34 +0300

    add pagination

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import css from './Button.module.css';
 import PropTypes from 'prop-types';
 
-const Button = ({ children, style, handler = null }) => {
+const Button = ({ children, style, type = 'button', handler = null }) => {
   return (
-    <div className={css[style]} onClick={handler}>
+    <button className={css[style]} onClick={handler} type={type}>
       {children}
-    </div>
+    </button>
   );
 };
 
@@ -15,4 +15,5 @@ Button.propTypes = {
   children: PropTypes.node.isRequired,
   style: PropTypes.string,
   handler: PropTypes.func,
+  type: PropTypes.string,
 };
