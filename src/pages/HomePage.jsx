fe7176a677f6217ee8612db53b0c2048d28311d9
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-19 16:36:33 +0300

    add phot to main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,24 @@
+import img from '../assets/campers.jpg';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
   return (
     <>
-      <h1>Welcome to Rental-Camper!</h1>
-      <p>
+      <h1 className={css.mainTitle}>Welcome to Rental-Camper!</h1>
+      <p className={css.mainText}>
         We offer comfortable rentals campers for your unforgettable vacation.
         Our houses on wheels equipped with a kitchen, sleeping unit and heating.
         You can rent camper from 2 days to several weeks. If you are considering
         buying a camper, rental is an ideal option for a test drive.
       </p>
-      <p>
+      <p className={css.mainText}>
         We invite you to visit our website: Rental-Camper to find out more about
         rental terms and our models of campers. We wish you wonderful travels!
         🚐✨
       </p>
+      <div className={css.mainImgContainer}>
+        <img src={img} alt="campers" />
+      </div>
     </>
   );
 };
